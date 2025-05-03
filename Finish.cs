a12eeb4b3a4dd20eec9feb9c88ea9015fd510a66
ShehabBryek <shehabbryek71@gmail.com>
2025-05-03 16:16:21 +0200

    Create Finish.cs

---
+++ Finish.cs
@@ -0,0 +1,20 @@
+
+
+using UnityEngine;
+ 
+public class Finish : MonoBehaviour
+{
+    public Stopwatch time;
+    public RoutePoint finishPoint;
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (finishPoint.isActiveForPlayer)
+        {
+            if (other.tag == "Player")
+            {
+                time.hasFinished = true;
+            }
+        }
+    }
+}
